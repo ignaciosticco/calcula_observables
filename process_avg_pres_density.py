7686cd840523151c28a6bf65d5fb354ef8d21793
ignaciosticco <ignaciosticco@gmail.com>
2019-05-31 17:57:55 -0300

    primera version completa de process_avg_pres_density.py

--- process_avg_pres_density.py
+++ process_avg_pres_density.py
@@ -1,10 +1,12 @@
 '''
 This script process the configuration and pressure values of each pedestrian and
-puts the values into a grid representing the room  
+outputs the number of pedestrians in the room, the avg pressure, max pressure,
+avg density and max density. The pressure is not normalized by the surface.   
 '''
 import pylab
 import numpy as np
 import math
+import pandas as pd
 
 ######################### PARAMETER #########################
 begin_corridor = 0.0
@@ -19,88 +21,87 @@ lenght_y = wall_up-wall_down
 
 ######################## Functions ######################## 
 
-def extract_config(data,n):
+
+def calculate_density(data):
      '''
-     n is the number of pedestrians in the room
-     this function extracts the configuration when the room has n pedestrians
-     only one timestep is extracted
+     this function extracts the configuration for each timestep
+     and calculates de mean density and max density for each timestep
      '''
-     str_n='{}\n'.format(n)
+     #str_n='{}\n'.format(n)
      f=open(data, 'r')
      lines=f.readlines()
-     index=[]
-     x=[]
-     y=[]
-     vx=[]
-     vy=[]
-     diameter=[]
      i=0
-     flag=True
-     while i<len(lines) and flag:
+     mean_density = []
+     max_density = []
+     while i<len(lines):
           line = lines[i].rstrip('\n')
           if(line=='ITEM: NUMBER OF ATOMS'):
-               number_pedestrians=lines[i+1]
-               if(number_pedestrians==str_n):
-                    number_pedestrians=int(number_pedestrians)
-                    first_line_table=i+7
-                    for j in range(first_line_table, first_line_table+number_pedestrians):
-                         row=lines[j].split(' ')
-                         index+=[int(row[0])]
-                         x+=[float(row[1])]
-                         y+=[float(row[2])]
-                         vx+=[float(row[3])]
-                         vy+=[float(row[4])]
-                         diameter+=[float(row[5])]
-                    flag=False
-          i+=1
-     f.close()
-     if i==len(lines):
-     	print("\nERROR: Number of pedestrians not found. Try a different number of pedestrians\n")
-     	exit()
-     return index,x,y,vx,vy,diameter
-
-
-def extract_observable(data,n):
-     '''
-     n is the number of pedestrians in the room
-     this function extracts the observables (pressure, work of friction) when the room has n pedestrians
-     only one timestep is extracted
-     '''
-     str_n='{}'.format(n)
-     f=open(data, 'r')
-     lines=f.readlines()
-     index=[]
-     pressure=[]
-     friction_work=[]
-     i=0
-     flag=True
-     while i<len(lines) and flag:
-          line = lines[i].rstrip('\n')  
-          if(line=='Cantidad de peatones = {} '.format(str_n)):
-               number_pedestrians= int(str_n)
-               first_line_table=i+2
-               for j in range(first_line_table, first_line_table+number_pedestrians):
+               index=[]
+               x=[]
+               y=[]
+               n=int(lines[i+1])
+               first_line_table=i+7
+               for j in range(first_line_table, first_line_table+n):
                     row=lines[j].split(' ')
                     index+=[int(row[0])]
-                    friction_work+=[float(row[1])]
-                    pressure+=[float(row[2])]
-               flag=False
+                    x+=[float(row[1])]
+                    y+=[float(row[2])]
+               density_matrix = create_density_matrx(x,y)
+               density_matrix[density_matrix == 0] = np.nan
+               mean_density += [np.nanmean(density_matrix)]
+               max_density += [int(np.nanmax(density_matrix))]
           i+=1
      f.close()
-     return index,friction_work,pressure
+     return mean_density,max_density
 
 
-def normaliza_por_n(grid_presion,grid_N):
+def calculate_pressure(data):
      '''
-     Esta funcion divide el valor de cada celda por la cantidad de peatones de cada celda. 
-     '''	
-     for i in range(0,len(grid_presion)):
-          for j in range(0,len(grid_presion[0])):
-               if grid_N[i,j] != 0:
-                    grid_presion[i,j]= float(grid_presion[i,j])/float(grid_N[i,j])
-               else:
-                    grid_presion[i,j]=0.0
-     return grid_presion
+     this function extracts the observables (pressure)
+     for every timestep returns a table with time, number pedestrians
+     avg pressure, max pressure
+     '''
+     f=open(data, 'r')
+     lines=f.readlines()
+     number_pedestrians = []
+     time = []
+     avg_pressure = []
+     max_pressure = []
+     i=0
+     while i<len(lines):
+          index=[]
+          pressure=[]
+          friction_work=[]
+          line = lines[i].rstrip('\n')
+          first_str = line.split(" ")[0] # first word of the line 
+          if(first_str=="Cantidad"):
+               n= int(line.split(" ")[4]) # n = number of pedestrians 
+               if n>0:
+                    t = lines[i+1].split(' ')[2] 
+                    first_line_table=i+2
+                    for j in range(first_line_table, first_line_table+n):
+                         row=lines[j].split(' ')
+                         index+=[int(row[0])]
+                         friction_work+=[float(row[1])]
+                         pressure+=[float(row[2])]
+                    number_pedestrians += [n]
+                    time += [t]
+                    avg_pressure += [np.mean(pressure)]
+                    max_pressure += [np.max(pressure)]
+          i+=1
+     f.close()
+     return time,number_pedestrians,avg_pressure,max_pressure
+
+def create_density_matrx(x,y):
+     grid_x = np.linspace(begin_corridor,end_corridor,end_corridor-begin_corridor)
+     grid_y = np.linspace(wall_down,wall_up,wall_up-wall_down)
+     density_matrix = np.zeros((len(grid_y),len(grid_x)))
+
+     for i in range(0,len(x)):
+          col=int((x[i]-begin_corridor)/(lenght_x)*(len(grid_x)))
+          fil = int((y[i]-wall_down)/(lenght_y)*(len(grid_y)))
+          density_matrix[fil][col] += 1
+     return density_matrix
 
 
 
@@ -114,28 +115,21 @@ def main():
 
      data_config = input("Enter configurations file name:\n")
      data_observables = input("Enter file with observables:\n") 
-     numer_of_pedestrians = input("Enter number of pedestrians:\n")
      output_file_name = input("Enter output file name:\n")
+     mean_density,max_density = calculate_density(data_config)
+     time,number_pedestrians,avg_pressure,max_pressure = calculate_pressure(data_observables)
 
-     index2,x,y,vx,vy,diameter=extract_config(data_config,numer_of_pedestrians)
-     index1,friction_work,presion = extract_observable(data_observables,numer_of_pedestrians)
      ################################################# 
-
-     grid_x = np.linspace(begin_corridor,end_corridor,end_corridor-begin_corridor)
-     grid_y = np.linspace(wall_down,wall_up,wall_up-wall_down)
-
-     grid_presion = np.zeros((len(grid_y),len(grid_x)))
-     grid_N = np.zeros((len(grid_y),len(grid_x)))
-
-     for i in range(0,len(presion)):
-          col=int((x[i]-begin_corridor)/(lenght_x)*(len(grid_x)))
-          fil = int((y[i]-wall_down)/(lenght_y)*(len(grid_y)))
-          grid_presion[fil][col] += presion[i]
-          grid_N[fil][col] += 1
-
-     grid_presion = normaliza_por_n(grid_presion,grid_N)
-     np.savetxt('{}'.format(output_file_name), grid_presion,fmt='%3.3f') 
-
+     d = {'time': time,
+          'number_pedestrians':number_pedestrians,
+          'avg_pressure':np.round(avg_pressure,2),
+          'max_pressure': np.round(max_pressure,2),
+          'max_density': np.round(max_density,2),
+          'mean_density': np.round(mean_density,2),
+          }
+     df = pd.DataFrame(data=d)
+     df.to_csv("{}".format(output_file_name), sep='\t',index = False
+    ,columns=["time","number_pedestrians","avg_pressure","max_pressure","max_density","mean_density"])
 
 if __name__=='__main__':
      main()
